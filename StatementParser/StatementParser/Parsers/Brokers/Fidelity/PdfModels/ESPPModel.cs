86c5da917549996001e59a25ec90a6c2dcd5a5cc
Catalin Ionut Fratila <cifratila@gmail.com>
2023-01-30 10:25:35 +0100

    Fix for Fidelity to handle double espp
    For some reason if you have more than 1 ESPP buys, it seems Fidelity PDF has one line with dollar sign before the amounts and one without.

--- StatementParser/StatementParser/Parsers/Brokers/Fidelity/PdfModels/ESPPModel.cs
+++ StatementParser/StatementParser/Parsers/Brokers/Fidelity/PdfModels/ESPPModel.cs
@@ -3,7 +3,7 @@ using ASoft.TextDeserializer.Attributes;
 
 namespace StatementParser.Parsers.Brokers.Fidelity.PdfModels
 {
-	[DeserializeByRegex("(?<Date>[0-9]{2}/[0-9]{2}/[0-9]{4})\\$(?<PurchasePrice>[0-9]+\\.[0-9]{5})\\$(?<MarketPrice>[0-9]+\\.[0-9]{3})(?<Amount>[0-9]+\\.[0-9]{3})")]
+	[DeserializeByRegex("(?<Date>[0-9]{2}/[0-9]{2}/[0-9]{4})\\$?(?<PurchasePrice>[0-9]+\\.[0-9]{5})\\$?(?<MarketPrice>[0-9]+\\.[0-9]{3})(?<Amount>[0-9]+\\.[0-9]{3})")]
 	internal class ESPPModel
 	{
 		public DateTime Date { get; set; }
