50641ec4ba6300a2425d4ae1cbc7889ca7d1e128
Vladimír Aubrecht <vladimir.aubrecht@me.com>
2025-03-29 01:05:11 +0100

    Added possibility to override yearly exchange rates from command line (#25)
    Co-authored-by: Vladimir Aubrecht <vlaub@microsoft.com>

---
+++ StatementParser/ExchangeRateProvider/Providers/Czk/StaticExchangeRateProvider.cs
@@ -0,0 +1,31 @@
+﻿using ExchangeRateProvider.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateProvider.Providers.Czk
+{
+    public class StaticExchangeRateProvider : IExchangeProvider
+    {
+        private readonly IDictionary<string, decimal> currencyToPriceDictionary;
+
+        public StaticExchangeRateProvider(IDictionary<string, decimal> currencyToPriceDictionary)
+        {
+            this.currencyToPriceDictionary = currencyToPriceDictionary;
+        }
+
+        public Task<ICurrencyList> FetchCurrencyListByDateAsync(DateTime date)
+        {
+            var currencyList = new List<CurrencyDescriptor>();
+
+            foreach (var currencyToPrice in currencyToPriceDictionary)
+            {
+                currencyList.Add(new CurrencyDescriptor(currencyToPrice.Key, "N/A", currencyToPrice.Value, "N/A"));
+            }
+
+            return Task.FromResult(new CurrencyList(currencyList) as ICurrencyList);
+        }
+    }
+}
--- StatementParser/TaxReporterCLI/Options.cs
+++ StatementParser/TaxReporterCLI/Options.cs
@@ -10,6 +10,9 @@ namespace TaxReporterCLI
         [Parameter("x", "excelSheetPath", Required = Required.No, Description = "Absolute or relative path to file where excel sheet will be stored.")]
         public string ExcelSheetPath { get; set; } = null;
 
+        [Parameter("r", "overrideYearlyExchangeRate", Required = Required.No, Description = "Yearly exchange rate which should be used instead of downloaded one. This is override useful in case yearly exchange rate is not published to proper place yet. Use json format: {'usd' = 23.0, 'czk' = 0.5}.")]
+        public string OverrideYearlyExchangeRate { get; set; } = null;
+
         [PositionalParameter(0, "inputStatementFilePath", Required = Required.Yes, Description = "Relative or absolute path to statement file or multiple paths each as one argument.")]
         [PositionalParameterList]
         public string[] StatementFilePaths { get; set; }
